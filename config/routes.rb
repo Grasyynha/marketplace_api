25d123a15f4a4434a90ce082bc85b7f522ce81c6
Grasiela Texeira <grasy.mst@gmail.com>
2024-01-08 16:00:27 -0300

    Instalação das Gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,3 +10,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+rails generate rspec:swagger API::MyController
