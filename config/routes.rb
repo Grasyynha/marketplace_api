f581c1f5fd2530fb41965773cdf09541f884fe24
Grasiela Texeira <grasy.mst@gmail.com>
2024-01-09 15:41:32 -0300

    Remove comments and add routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json }, 
                   constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
 
+    end                
   end
+  
 end
